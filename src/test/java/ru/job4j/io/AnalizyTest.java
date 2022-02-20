2a920443bfa8f736cb7d804a09ee5571fc633d76
alex-vet <alex157@inbox.ru>
2022-02-20 19:18:37 +0400

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,35 +1,54 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.util.Collections;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void whenTwoPeriods() {
+    public void whenTwoPeriods() throws IOException {
         Analizy log = new Analizy();
-        log.unavailable("./data/log1.csv", "./data/unavailable.csv");
-        List<String> out = List.of("10:57:01;10:59:01;", "11:01:02;11:02:02;");
-        assertThat(log.load("./data/unavailable.csv"), is(out));
+        File target = folder.newFile("unavailable.csv");
+        log.unavailable("./data/log1.csv", target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;10:59:01;11:01:02;11:02:02;"));
     }
 
     @Test
-    public void whenOnlyOnePeriod() {
+    public void whenOnlyOnePeriod() throws IOException {
         Analizy log = new Analizy();
-        log.unavailable("./data/log2.csv", "./data/unavailable.csv");
-        List<String> out = List.of("10:57:01;11:02:02;");
-        assertThat(log.load("./data/unavailable.csv"), is(out));
+        File target = folder.newFile("unavailable.csv");
+        log.unavailable("./data/log2.csv", target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("10:57:01;11:02:02;"));
     }
 
     @Test
-    public void whenZeroPeriods() {
+    public void whenZeroPeriods() throws IOException {
         Analizy log = new Analizy();
-        log.unavailable("./data/log3.csv", "./data/unavailable.csv");
-        assertThat(log.load("./data/unavailable.csv"), is(Collections.emptyList()));
+        File target = folder.newFile("unavailable.csv");
+        log.unavailable("./data/log3.csv", target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is(""));
     }
 }
